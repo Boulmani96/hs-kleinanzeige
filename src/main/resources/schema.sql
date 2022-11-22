2f1278bedc7308e0552c2ea0dbef8ef87564a52a
Boulmani96 <ouassimeboulmani96@gmail.com>
2022-11-22 22:22:41 +0100

    try to fix Pipeline

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ drop table if exists AD;
 
 drop table if exists CATEGORY;
 
-drop table if exists USER;
+drop table if exists USER CASCADE;
 
 create table if not exists CATEGORY
 (
@@ -13,10 +13,24 @@ create table if not exists CATEGORY
     FOREIGN key (PARENT_ID) REFERENCES CATEGORY(ID)
 );
 
+create table if not exists USER
+(
+    ID         INTEGER AUTO_INCREMENT ,
+    EMAIL      VARCHAR(60) not null ,
+    PASSWORD   VARCHAR(60) comment '' ,
+    FIRST_NAME varchar(60) comment '',
+    LAST_NAME  varchar(60) comment '',
+    PHONE      varchar(60) comment '',
+    LOCATION   varchar(60) comment '',
+    CREATED    TIMESTAMP comment '',
+    primary key (ID)
+);
+
 create table if not exists AD
 (
     ID                 INTEGER AUTO_INCREMENT comment '',
     CATEGORY_ID        INTEGER not null DEFAULT '0' comment '',
+    USER_ID            INTEGER not null DEFAULT '0' comment '',
     TYPE               ENUM('OFFER', 'REQUEST') not null comment '',
     TITLE              varchar(60) not null comment '',
     DESCRIPTION        varchar(60) not null comment '',
@@ -24,5 +38,6 @@ create table if not exists AD
     LOCATION           varchar(60) comment '',
     CREATED            TIMESTAMP comment '',
     primary key (ID),
-    FOREIGN KEY (CATEGORY_ID) REFERENCES CATEGORY(ID)
+    FOREIGN KEY (CATEGORY_ID) REFERENCES CATEGORY(ID),
+    FOREIGN KEY (USER_ID) REFERENCES USER(ID)
 );
