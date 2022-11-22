974ca708a82b513b4275426ca85ccedd473b1cdd
Boulmani96 <ouassimeboulmani96@gmail.com>
2022-11-22 22:06:18 +0100

    try to fix Pipeline

--- src/main/java/de/hs/da/hskleinanzeigen/HsKleinanzeigenApplication.java
+++ src/main/java/de/hs/da/hskleinanzeigen/HsKleinanzeigenApplication.java
@@ -3,8 +3,10 @@ package de.hs.da.hskleinanzeigen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Controller;
 
+@EnableJpaAuditing
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @Controller
 public class HsKleinanzeigenApplication {
