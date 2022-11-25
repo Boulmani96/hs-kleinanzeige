7c95cf0667d20dcdae52f47e7ce82cf9716e60d7
steltaib <el-houssine.taibi@stud.h-da.de>
2022-11-25 14:26:36 +0100

    fix Type

--- src/main/java/de/hs/da/hskleinanzeigen/controller/AdvertisementController.java
+++ src/main/java/de/hs/da/hskleinanzeigen/controller/AdvertisementController.java
@@ -3,6 +3,7 @@ package de.hs.da.hskleinanzeigen.controller;
 import de.hs.da.hskleinanzeigen.domain.AD;
 import de.hs.da.hskleinanzeigen.domain.Category;
 import de.hs.da.hskleinanzeigen.domain.NotFoundException;
+import de.hs.da.hskleinanzeigen.domain.Type;
 import de.hs.da.hskleinanzeigen.domain.User;
 import de.hs.da.hskleinanzeigen.repository.AdvertisementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
