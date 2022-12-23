488b0395acc727bbb585247942a6b8b37ca38293
steltaib <el-houssine.taibi@stud.h-da.de>
2022-12-23 17:04:23 +0100

    add more tests for NotepadControllerTest

--- src/test/java/de/hs/da/hskleinanzeigen/controller/NotepadControllerTest.java
+++ src/test/java/de/hs/da/hskleinanzeigen/controller/NotepadControllerTest.java
@@ -5,14 +5,19 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import de.hs.da.hskleinanzeigen.domain.AD;
 import de.hs.da.hskleinanzeigen.domain.Notepad;
 import de.hs.da.hskleinanzeigen.domain.User;
 import de.hs.da.hskleinanzeigen.dtos.AdDTO;
 import de.hs.da.hskleinanzeigen.dtos.CreationNotepadDTO;
+import de.hs.da.hskleinanzeigen.dtos.GetNotepadDTO;
 import de.hs.da.hskleinanzeigen.dtos.NotepadDTO;
 import de.hs.da.hskleinanzeigen.dtos.UserDTO;
 import de.hs.da.hskleinanzeigen.mappers.NotepadMapper;
 import de.hs.da.hskleinanzeigen.services.NotepadService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +27,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -119,5 +125,37 @@ public class NotepadControllerTest {
     // Verify that the returned value is as expected
     assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
   }
+  @Test
+  public void testGetNotepad() throws Exception {
+    CreationNotepadDTO creationNotepadDTO = new CreationNotepadDTO(1,"note");
+    Notepad notepad1 = mock(Notepad.class);
+    Notepad notepad2 = mock(Notepad.class);
+    UserDTO user= mock(UserDTO.class);
+    List<Notepad> notepadList = new ArrayList<>();
+    notepadList.add(notepad1);
+    notepadList.add(notepad2);
+//    when(notepadService.findByUser_idAndByAd_id(1, 1)).thenReturn(notepad1);
+    when(userController.getUserByID(1)).thenReturn(ResponseEntity.ok(user));
+    when(notepadService.findNotepadByUser_id(1)).thenReturn(notepadList);
+
+    // Call the putNotepad() method on the NotepadController instance
+    ResponseEntity<List<GetNotepadDTO>> response = notepadController.getNotepad(1);
+
+    // Verify that the returned value is as expected
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+  @Test
+  public void testGetNotepad_NO_CONTENT() throws Exception {
+
+    UserDTO user= mock(UserDTO.class);
+    when(userController.getUserByID(1)).thenReturn(ResponseEntity.ok(user));
+
+    // Call the putNotepad() method on the NotepadController instance
+    ResponseEntity<List<GetNotepadDTO>> response = notepadController.getNotepad(1);
+
+    // Verify that the returned value is as expected
+    assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+  }
+
 
 }
