bef55c031a0771ab1b934f96a32612750664c9fb
Boulmani96 <ouassimeboulmani96@gmail.com>
2023-01-19 15:56:51 +0100

    add Enablecaching to project

--- src/main/java/de/hs/da/hskleinanzeigen/HsKleinanzeigenApplication.java
+++ src/main/java/de/hs/da/hskleinanzeigen/HsKleinanzeigenApplication.java
@@ -3,10 +3,12 @@ package de.hs.da.hskleinanzeigen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @Controller
+@EnableCaching
 public class HsKleinanzeigenApplication {
     public static void main(String[] args) {
         SpringApplication.run(HsKleinanzeigenApplication.class, args);
